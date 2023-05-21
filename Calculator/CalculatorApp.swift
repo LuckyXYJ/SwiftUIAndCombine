bda59493ebb725f8e6359b0f026a587addd66303
luckyxyj <470446087@qq.com>
2023-05-21 18:45:14 +0800

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by LuckyXYJ on 2023/5/21.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
