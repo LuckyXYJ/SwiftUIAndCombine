1bbff7d21a279ad1e7db57ba5c80ec48deb0062d
luckyxyj <470446087@qq.com>
2023-05-23 08:44:30 +0800

    submit

--- Calculator/Calculator/ContentView.swift
+++ Calculator/Calculator/ContentView.swift
@@ -8,34 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
-    var value = 13
-    
-    let pad: [[CalculatorButtonItem]] = [
-        [.command(.clear), .command(.flip), .command(.percent), .op(.divide)],
-        [.digit(7), .digit(8), .digit(9), .op(.multiply)],
-        [.digit(4), .digit(5), .digit(6), .op(.minus)],
-        [.digit(1), .digit(2), .digit(3), .op(.plus)],
-        [.digit(0), .dot, .op(.equal)]
-    ]
     
     var body: some View {
-        VStack(spacing: 8) {
-            ForEach(pad, id: \.self) { row in
-                CalculatorButtonRow(row: row)
-            }
+        VStack(spacing: 12) {
+            Text("0")
+                .font(.system(size: 76))
+                .minimumScaleFactor(0.5)
+                .padding(.trailing, 24)
+                .lineLimit(1)
+                .frame(minWidth: 0, maxWidth: .infinity, alignment: .trailing)
+            
+            CalculatorButtonPad()
+                .padding(.bottom)
         }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+            ContentView().previewDevice("iPhone 4S")
+        }
+        
     }
 }
 
 struct CalculatorButton: View {
     
-    let fontSize: CGFloat = 18
+    let fontSize: CGFloat = 28
     
     let title: String
     
@@ -74,3 +75,19 @@ struct CalculatorButtonRow: View {
     }
 }
 
+struct CalculatorButtonPad: View {
+    let pad: [[CalculatorButtonItem]] = [
+        [.command(.clear), .command(.flip), .command(.percent), .op(.divide)],
+        [.digit(7), .digit(8), .digit(9), .op(.multiply)],
+        [.digit(4), .digit(5), .digit(6), .op(.minus)],
+        [.digit(1), .digit(2), .digit(3), .op(.plus)],
+        [.digit(0), .dot, .op(.equal)],
+    ]
+    var body: some View {
+        VStack(spacing: 8) {
+            ForEach(pad, id: \.self) { row in
+                CalculatorButtonRow(row: row)
+            }
+        }
+    }
+}
